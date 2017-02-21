06dfb4e9f1566c8a320e3965907059b3d6dcb634
Никита Боначев < >
2017-02-22 01:17:13 +0500

    init

---
+++ docker/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -n "${DEV_UID}" ]; then
+    usermod -u ${DEV_UID} www-data
+fi
+if [ -n "${DEV_GID}" ]; then
+    groupmod -g ${DEV_GID} www-data
+fi
+
+chown -R www-data:www-data /data
+ulimit -s unlimited
+rm -rf '/var/run/apache2'
+service apache2 start
