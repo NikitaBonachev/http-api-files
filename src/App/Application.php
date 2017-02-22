6e6458cf82dbcd77d33f4482da5b9ff63279829c
Nikita Bonachev <nbonachev@gmail.com>
2017-02-22 22:57:00 +0500

    add: db

--- src/App/Application.php
+++ src/App/Application.php
@@ -11,5 +11,21 @@ class Application extends SilexApplication
         parent::__construct();
         $app = $this;
         $app->mount('', new ControllerProvider());
+
+        $config = new \Doctrine\DBAL\Configuration();
+
+        $connectionParams = array(
+            'dbname' => 'xsolla_test',
+            'user' => 'api',
+            'password' => '21333',
+            'host' => 'localhost',
+            'driver' => 'pdo_mysql',
+        );
+        $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+        $conn->executeQuery("SHOW TABLES");
+
+        var_dump($conn->executeQuery("SHOW TABLES")->fetchAll());
+
     }
 }
