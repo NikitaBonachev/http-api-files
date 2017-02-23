b5324edc5f07aa23921ef4d682a71d9dc5abae69
Nikita Bonachev <nbonachev@gmail.com>
2017-02-23 12:51:00 +0500

    unstable

--- tests/App/Data/DataManagerTests.php
+++ tests/App/Data/DataManagerTests.php
@@ -1,14 +1,34 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-require_once __DIR__.'/../../../src/App/Data/DataManager.php';
 require_once __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__.'/../../../src/App/Data/DataManager.php';
+require_once __DIR__.'/../../../src/App/Application.php';
+
+use PHPUnit\Framework\TestCase;
 
 class DataManagerTests extends TestCase {
 
     public function testClassCreated()
     {
-        $dataProvider = new \App\Data\DataManager();
+
+        $app = new App\Application('dev');
+        $app->register(
+            new \Silex\Provider\DoctrineServiceProvider(),
+            [
+                'db.options' =>
+                    [
+                        'dbname' => 'xsolla_test',
+                        'user' => 'api',
+                        'password' => '21333',
+                        'host' => 'localhost',
+                        'driver' => 'pdo_mysql'
+                    ]
+            ]
+        );
+
+
+        $db = $app['db'];
+        $dataProvider = new \App\Data\DataManager($db);
         $this->assertNotNull($dataProvider);
     }
 
