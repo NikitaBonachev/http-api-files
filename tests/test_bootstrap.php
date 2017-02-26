4d542d7764edc8196b2b7c97ebb576304102b123
Nikita Bonachev <nbonachev@gmail.com>
2017-02-26 22:13:13 +0500

    add: caught exception

--- tests/test_bootstrap.php
+++ tests/test_bootstrap.php
@@ -11,15 +11,19 @@ $app_env = 'test';
 $app = new App\Application($app_env);
 $app['env'] = $app_env;
 
-$app->register(
-    new DoctrineServiceProvider(),
-    [
-        'db.options' => \App\Config\ConfigProvider::getDatabaseConfig($app_env)
-    ]
-);
+try {
+    $app->register(
+        new DoctrineServiceProvider(),
+        [
+            'db.options' => \App\Config\ConfigProvider::getDatabaseConfig($app_env)
+        ]
+    );
 
-if (!is_dir(\App\Config\ConfigProvider::getUploadDir($app_env))) {
-    mkdir(\App\Config\ConfigProvider::getUploadDir($app_env));
+    if (!is_dir(\App\Config\ConfigProvider::getUploadDir($app_env))) {
+        mkdir(\App\Config\ConfigProvider::getUploadDir($app_env));
+    }
+} catch (Exception $e) {
+    // Exception will be caught in application
 }
 
 return $app;
